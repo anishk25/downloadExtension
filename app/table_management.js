97f6694ed060bdf20e25d3751e7a4c941c9f3f3e
anishk25 <akhattar@umd.edu>
2014-01-23 17:54:37 -0500

    made table rows editable and fixed issues with local memory

--- app/table_management.js
+++ app/table_management.js
@@ -15,24 +15,61 @@ $(document).ready(function(){
 		localStorage.clear();
 		mytable.innerHTML = '<tr><td>URLS</td><td>Filepath</td></tr>';		
 		return false;
-	});	
-				
+	});
+	
+	$(document).on('click', '#selectFolder', function(event){		
+		connectToNative();
+		return false;
+	});
+	
+	var oldURL;	
+	$(document).on('dblclick','.url_col',function(event){
+		var table_row = $(this).closest('tr');
+		var url_name = table_row.find(".url_col").text();
+		oldURL = url_name;			
+	});
+	
+	
+	$(document).on('blur','.url_col',function(event){
+		var table_row = $(this).closest('tr');
+		var url_name = table_row.find(".url_col").text();
+		var folder_path = table_row.find(".folder_col").text();
+		localStorage.removeItem(oldURL);
+		localStorage.setItem(url_name,folder_path);			
+		return false;
+	});
+	
+	$(document).on('blur','.folder_col',function(event){
+		var table_row = $(this).closest('tr');
+		var url_name = table_row.find(".url_col").text();
+		var folder_path = table_row.find(".folder_col").text();		
+		localStorage.setItem(url_name,folder_path);			
+		return false;
+	});			
 				
 	$('#addrow').click(function(){
 		var urlName = document.getElementById("urlBox").value;
-		var folder = document.getElementById("folderBox").value;
+		var folder = document.getElementById("folderBox").value;	
+		
 		if(urlName == null || urlName == "" || folder == null || folder == ""){
 			alert("Must fill out website and folder name");
 			return false;
 		}
 		
-		// storing url and folder in local storage
-		var filePath = folder.replace("\\","\\\\");
-		localStorage.setItem(urlName,filePath);	
+		// storing url and folder in local storage		
+		localStorage.setItem(urlName,folder);	
 
 		// adding row in table
-		$('#mytable').append('<tr><td class = "url_col">' + urlName + '</td><td class = "folder_col">' + folder + '</td><td><button class = "dltbtn">delete</button></td></tr>');
-		localStorage.setItem('table', mytable.innerHTML);		
+		$('#mytable').append('<tr><td class = "url_col" contenteditable="true" >' + urlName + '</td><td class = "folder_col" contenteditable="true" >' + folder + '</td><td><button class = "dltbtn">delete</button></td></tr>');
+		localStorage.setItem('table', mytable.innerHTML);	
+		
+		var length = localStorage.length;
+		for(var i = 0;i < length; i++){
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);	
+			console.log("key: " + key + " value: " + value);				
+		}
+			
 		return false;
 	});
 	
@@ -41,6 +78,23 @@ $(document).ready(function(){
 		if ( localStorage.getItem('table')) {
 			mytable.innerHTML = localStorage.getItem('table'); 			
 		}		
+	}
+	
+	function connectToNative(){
+		console.log('connecting to native app');
+		var port = chrome.runtime.connectNative("com.google.chrome.example.nativefoldercreator");	
+		port.postMessage({commandType:"browseFolders"});		
+		port.onMessage.addListener(function(msg) {				
+			var parsed = JSON.parse(JSON.stringify(msg));
+			var folderPath = parsed.folderPath;
+			var fileForm = document.getElementById('folderBox');
+			fileForm.value = folderPath;
+			console.log("Received: " + folderPath);
+		});
+		
+		port.onDisconnect.addListener(function() {
+			console.log("Disconnected");
+		});	
 	}
 	
 });
